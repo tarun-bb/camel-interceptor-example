a3a2f364dab09021ac454b247d03962647103dd3
Tarun Bharti <tarun@backbase.com>
2021-10-09 03:35:13 +0200

    added readme file

--- src/main/java/com/camel/interceptor/restdsl/RestDslRouteBuilder.java
+++ src/main/java/com/camel/interceptor/restdsl/RestDslRouteBuilder.java
@@ -18,7 +18,7 @@ public class RestDslRouteBuilder extends RouteBuilder {
     public void configure() {
 
         interceptSendToEndpoint(SAMPLE_ENDPOINT)
-            .log(LoggingLevel.INFO, "checking for AFTER_URI")
+            .log(LoggingLevel.INFO, "Interceptor triggerred for SAMPLE_ENDPOINT to check condition for AFTER_URI")
             .afterUrl(AFTER_URI_PROCESSOR);
 
         restConfiguration()
@@ -37,8 +37,9 @@ public class RestDslRouteBuilder extends RouteBuilder {
             .to(LOCAL_SERVICE);
 
         from(LOCAL_SERVICE).routeId(ROUTE_ID)
-            .log(LoggingLevel.INFO, "Inside direct:localService")
+            .log(LoggingLevel.INFO, "Inside " + ROUTE_ID + ", now Calling SAMPLE_ENDPOINT")
             .to(SAMPLE_ENDPOINT)
+            .log(LoggingLevel.INFO, "Returning final response")
             .to("bean:getBean"); // This will invoke the Spring bean 'getBean'*/
 
         from(SAMPLE_ENDPOINT)
@@ -47,7 +48,9 @@ public class RestDslRouteBuilder extends RouteBuilder {
         from(AFTER_URI_PROCESSOR)
             .choice()
                 .when(header(AFTER_URI).isEqualTo("true"))
-                .log(LoggingLevel.INFO, "afterURI condition met, now executing the logic")
+                .log(LoggingLevel.INFO, "afterURI was true, now executing the afterURI logic")
+            .otherwise()
+                .log(LoggingLevel.INFO, "afterURI condition not met")
             .endChoice();
 
     }
